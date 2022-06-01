ec97abc85d1f72e88838517cbd3110a8f39bda44
Claudio Cruz <claudiocruz1989@gmail.com>
2022-06-01 15:36:28 +0000

    Add changeSnakePosition and keyDown functions to script.js and add style to the header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById(game-field);
+const canvas = document.getElementById('game-field');
 const ctx = canvas.getContext('2d');
 
 let speed = 7;
@@ -8,9 +8,13 @@ let tileSize = canvas.width / tileCount - 2;
 let headX = 10;
 let headY = 10;
 
+let xVelocity = 0;
+let yVelocity = 0;
+
 //game loop
 function drawGame() {
     clearScreen();
+    changeSnakePosition();
     drawSnake();
     setTimeout(drawGame, 1000 / speed);
 }
@@ -23,6 +27,47 @@ function clearScreen() {
 function drawSnake() {
     ctx.fillStyle = 'green';
     ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+}
+
+function changeSnakePosition() {
+    headX = headX + xVelocity;
+    headY = headY + yVelocity;
+}
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event) {
+    //up
+    if(event.keyCode == 38){
+        if (yVelocity == 1)
+            return;
+        yVelocity = -1;
+        xVelocity = 0;
+    }
+
+    //down
+    if(event.keyCode == 40){
+        if (yVelocity == -1)
+            return;
+        yVelocity = 1;
+        xVelocity = 0;
+    }
+
+    //left
+    if(event.keyCode == 37){
+        if (xVelocity == 1)
+            return;
+        yVelocity = 0;
+        xVelocity = -1;
+    }
+
+    //right
+    if(event.keyCode == 39){
+        if (xVelocity == -1)
+            return;
+        yVelocity = 0;
+        xVelocity = 1;
+    }
 }
 
 drawGame();
